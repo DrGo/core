7e2a84f521c37410ec0daee1e19ecfc8e0c1d1f6
DrGo <salah.mahmud@gmail.com>
2021-02-12 12:23:29 -0600

    added template using support

--- template/template_test.go
+++ template/template_test.go
@@ -1,110 +1,92 @@
-package extemplate
+package template
 
 import (
-	"bytes"
-	"html/template"
-	"strings"
-	"sync"
+	"fmt"
+	"log"
+	"os"
 	"testing"
+
+	"github.com/matryer/is"
 )
 
-var x *Extemplate
-var once sync.Once
 
-func setup() {
-	x = New().Delims("{{", "}}").Funcs(template.FuncMap{
-		"tolower": strings.ToLower,
-	})
-	err := x.ParseDir("examples/", []string{".tmpl"})
+
+
+var layout = `
+<html>
+  <head>
+    <title>{{.Title}}</title>
+  </head>
+  <body>
+    <div>{{ template "Content" . }}</div>
+  </body>
+</html>`
+
+var page1 = `
+{{/* using "folder/file.ext" */}}
+"{{ .Message }}" from Page 1`
+
+var page2 = `
+"{{ .Message }}" from Page 2`
+
+
+func TestExtractUsingPaths(t *testing.T){
+  f, err := os.Open("examples/parent.tmpl")
+  if err!= nil {
+     t.Fatalf("%v", err)
+  }
+  defer f.Close()
+  files, err := extractUsingPaths(f)
+  is := is.New(t)
+  is.NoErr(err)
+  if len(files) == 0 {
+    t.Fatal("empty files")
+  }
+  is.Equal(files[0],"folder/file.ext")
+  t.Logf("%s", files[0])
+}
+
+func ignoreExampleTemplate() {
+	data := struct {
+		Title   string
+		Message string
+	}{
+		Title:   "Some Title",
+		Message: "Hello, World",
+	}
+
+	parser := &Parser{layout: layout}
+	output1, err := parser.Parse(page1, data)
 	if err != nil {
-		panic(err)
-	}
-}
-
-func TestLookup(t *testing.T) {
-	once.Do(setup)
-
-	if tmpl := x.Lookup("foobar"); tmpl != nil {
-		t.Errorf("Lookup: expected nil, got %#v", tmpl)
+		log.Fatal(err)
 	}
 
-	if tmpl := x.Lookup("child.tmpl"); tmpl == nil {
-		t.Error("Lookup: expected template, got nil")
-	}
-}
+	fmt.Println("Page 1 output: ", output1)
 
-func TestExecuteTemplate(t *testing.T) {
-	once.Do(setup)
-
-	var buf bytes.Buffer
-	if err := x.ExecuteTemplate(&buf, "child.tmpl", nil); err != nil {
-		t.Errorf("ExecuteTemplate: %s", err)
-	}
-	if err := x.ExecuteTemplate(&buf, "foobar", nil); err == nil {
-		t.Error("ExecuteTemplate: expected err for unexisting template, got none")
+	output2, err := parser.Parse(page2, data)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-}
+	fmt.Println("Page 2 output: ", output2)
 
-func TestTemplates(t *testing.T) {
-	once.Do(setup)
-
-	tests := map[string]string{
-		"parent.tmpl":      "Hello from master.tmpl",                                     // normal template with {{ block }}
-		"child.tmpl":       "Hello from child.tmpl\n\tHello from partials/question.tmpl", // template with inheritance
-		"grand-child.tmpl": "Hello from grand-child.tmpl",                                // template with nested inheritance
-	}
-
-	for k, v := range tests {
-		tmpl := x.Lookup(k)
-		if tmpl == nil {
-			t.Errorf("template not found in set: %s", k)
-		}
-
-		var buf bytes.Buffer
-		if err := tmpl.Execute(&buf, nil); err != nil {
-			t.Errorf("error executing template %s: %s", k, err)
-		}
-
-		e := strings.TrimSpace(buf.String())
-		if e != v {
-			t.Errorf("incorrect template result. \nExpected: %s\nActual: %s", v, e)
-		}
-	}
-}
-
-func TestNewTemplateFile(t *testing.T) {
-	tests := map[string]string{
-		"{{ extends \"foo.html\" }}": "foo.html",
-		"Nothing":                    "",
-		"{{ extends \"dir/file.html\" }}\n {{ .Var }}": "dir/file.html",
-	}
-
-	for c, e := range tests {
-		tf, err := newTemplateFile([]byte(c))
-		if err != nil {
-			t.Error(err)
-		}
-		if tf.layout != e {
-			t.Errorf("Expected layout %s, got %s", e, tf.layout)
-		}
-	}
-}
-
-func BenchmarkExtemplateGetLayoutForTemplate(b *testing.B) {
-	c := []byte("{{ extends \"foo.html\" }}")
-	for i := 0; i < b.N; i++ {
-		if _, err := newTemplateFile(c); err != nil {
-			b.Error(err)
-		}
-	}
-}
-
-func BenchmarkExtemplateParseDir(b *testing.B) {
-	x := New().Funcs(template.FuncMap{
-		"foo": strings.ToLower,
-	})
-	for i := 0; i < b.N; i++ {
-		x.ParseDir("examples/", []string{".tmpl"})
-	}
+	// Output:
+	// Page 1 output:
+	// <html>
+	//   <head>
+	//     <title>Some Title</title>
+	//   </head>
+	//   <body>
+	//     <div>"Hello, World" from Page 1</div>
+	//   </body>
+	// </html>
+	// Page 2 output:
+	// <html>
+	//   <head>
+	//     <title>Some Title</title>
+	//   </head>
+	//   <body>
+	//     <div>"Hello, World" from Page 2</div>
+	//   </body>
+	// </html>
 }
